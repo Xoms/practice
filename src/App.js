9bb790cf8443d047275fd76896db752d2854c1cf
Julia-itcourse <juliaartemenko93@gmail.com>
2020-11-26 17:26:23 +0100

    started sign-in form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './App.css';
 import Header from './components/Header';
 import Container from './components/shared/Container/Container';
 import SignUp from './components/SignUp/SignUp';
+import SignIn from './components/SignIn/SignIn';
 
 const users = [
   {
-    name: '',
-    email: '',
-    password: '',
-    passwordRepeat: ''
+    name: 'john snow',
+    email: 'john@mail.ru',
+    password: '1',
+    passwordRepeat: '1'
+  },
+  {
+    name: 'arya stark',
+    email: 'arya@mail.ru',
+    password: '2',
+    passwordRepeat: '2'
   }
 ]
 function App() {
@@ -19,6 +26,7 @@ function App() {
       <Header />
       <Container>
         <SignUp />
+        <SignIn users = {users}/>
       </Container>
     </>
   );
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-//import { Test } from './SignIn.styles';
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import Input from "../shared/InputField"
+import Form from "../shared/Form"
 
-const SignIn = (props) => (
-  <div className="SignInWrapper">
-    Test content
-  </div>
-);
+class SignIn extends Component {
+
+  constructor(props){
+this.props = props;
+  }
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange=({target})=>{
+    const {name, value} = target;
+    this.setState({
+      [name]:value,
+    })
+  }
+  
+handleSubmit=(event)=>{
+  event.preventDefault();
+ console.log(this.props); 
+
+}
+
+
+
+  render() {
+    return (
+      <Form title = "Sign In" onSubmit = {this.handleSubmit}>
+        <Input title = "Email" type = "email" name = "email" value={this.state.email} onChange = {this.handleChange}/>
+        <Input title = "Password" type = "password" name="password" value={this.state.password} onChange  = {this.handleChange}/>
+      </Form>
+    )
+  }
+}
+
+// const SignIn = (props) => (
+//   <div className="SignInWrapper">
+//     Test content
+//   </div>
+// );
 
 SignIn.propTypes = {
   // bla: PropTypes.string,
-};
+}
 
 SignIn.defaultProps = {
   // bla: 'test',
-};
+}
 
-export default SignIn;
+export default SignIn
